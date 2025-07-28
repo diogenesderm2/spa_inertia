2d54791ec22f7a0cb12807efd9a089728d05d2ac
gdr-ti <admin@gdr.com.br>
2025-07-28 13:58:36 -0300

    first commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(2),
+            'price' => fake()->randomFloat(2, 10, 1000),
+            'description' => fake()->text(200),
+            'weight' => fake()->randomFloat(2, 10, 1000),
+            'brand' => fake()->sentence(2),
+        ];
+    }
+}
